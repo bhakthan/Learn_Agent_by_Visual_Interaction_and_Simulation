de7163aa798988daf04d92995357b092fdc2c89c
SB <bhakthan@users.noreply.github.com>
2025-07-06 16:07:06 +0000

    deleting obsolete files

--- test.js
+++
@@ -1,3 +0,0 @@
-// Simple test script to see if agentResponse.includes("```") is valid syntax
-const testString = "This is a test ```code block``` end";
-console.log(testString.includes("```"));  // Should print true
